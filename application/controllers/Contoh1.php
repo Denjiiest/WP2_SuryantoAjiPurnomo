963e159e2d4d56fa9771601909bdb53d1426cc9e
Denjiiest <jimenk123@gmail.com>
2022-03-29 01:32:03 +0700

    pustaka-booking suryanto

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h>Perkenalkan</h1>";
+        echo" Nama saya Suryanto Aji Purnomo
+              Saya tinggal di daerah Jakarta Utara
+              Olahraga yang saya sukai adalah
+              Bulutangkis";
+
+    }
+}
