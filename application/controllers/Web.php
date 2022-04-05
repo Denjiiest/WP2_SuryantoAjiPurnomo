419984bf4a2071eeaf823b223daf7b7e5bcbc40c
Denjiiest <jimenk123@gmail.com>
2022-04-05 15:08:24 +0700

    menambahkan controller/web.php, views/v_about.php,v_footer.php,v_header.php,v_index.php, dan asset css

---
+++ application/controllers/Web.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') or exit('no direct script access allowed');
+
+class Web extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+    public function about()
+    {
+        $data['judul'] = "Halaman about";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Suryanto Aji</li>
+ <li>Nama Belakang : Purnomo </li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Budimulia</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Sepakbola</li>
+ <li>BuluTangkis</li>
+ <li>Berenang</li>
+ </ul>
+</section>
